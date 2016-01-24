4eca8693eb0642b6a21219c2f742c927466eb7ed
ocoulson <o_coulson@hotmail.co.uk>
2016-01-24 20:51:43 +0000

    Semi-finished adapter class exercise - not sure what to do for the methods in Xpay which refer to something not contained in PayD, throw an exception perhaps.

--- Day3/src/AdapterPattern/XpayToPayDAdapter.java
+++ Day3/src/AdapterPattern/XpayToPayDAdapter.java
@@ -4,68 +4,80 @@ package AdapterPattern;
  * Created by Oliver Coulson on 22/01/2016.
  */
 public class XpayToPayDAdapter implements Xpay {
+    private PayD payD;
+
+    public XpayToPayDAdapter(PayD payD) {
+        this.payD = payD;
+    }
+
     @Override
     public String getCreditCardNo() {
-        return null;
+
+         return payD.getCustCardNo();
     }
 
     @Override
     public void setCreditCardNo(String creditCardNo) {
 
+        payD.setCustCardNo(creditCardNo);
     }
 
     @Override
     public String getCustomerName() {
-        return null;
+        return payD.getCardOwnerName();
     }
 
     @Override
     public void setCustomerName(String customerName) {
-
+        payD.setCardOwnerName(customerName);
     }
 
     @Override
     public String getCardExpMonth() {
-        return null;
+
+        return payD.getCardExpMonthDate();
     }
 
     @Override
     public void setCardExpMonth(String cardExpMonth) {
-
+        payD.setCardExpMonthDate(cardExpMonth);
     }
 
     @Override
     public String getCardExpYear() {
+//        throw new IllegalAccessException("Card Expiry Year not found in PayD objects");
         return null;
     }
 
     @Override
     public void setCardExpYear(String cardExpYear) {
+//        throw new IllegalAccessException("Card Expiry Year not found in PayD objects");
 
     }
 
     @Override
     public Short getCardCVVNo() {
-        return null;
+        return payD.getCVVNo().shortValue();
     }
 
     @Override
     public void setCardCVVNo(Short cardCVVNo) {
-
+        payD.setCVVNo(cardCVVNo.intValue());
     }
 
     @Override
     public Double getAmount() {
-        return null;
+        return payD.getTotalAmount();
     }
 
     @Override
     public void setAmount(Double amount) {
-
+        payD.setTotalAmount(amount);
     }
 
     @Override
     public String toString() {
-        return super.toString();
+        return "" + payD.getCustCardNo() + " " + payD.getCardOwnerName() + " " + payD.getCardExpMonthDate()
+                + " " + payD.getCVVNo() + " " + payD.getTotalAmount();
     }
 }
