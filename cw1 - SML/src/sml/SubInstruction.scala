4582d143410006815d1aadafb45a3b2a4544651e
ocoulson <o_coulson@hotmail.co.uk>
2016-03-01 16:41:20 +0000

    Added the SubInstruction subclass to SML - Basically a clone of the AddInstruction class, but with - instead of +

---
+++ cw1 - SML/src/sml/SubInstruction.scala
@@ -0,0 +1,23 @@
+package sml
+
+/**
+  * Created by Oliver Coulson on 01/03/2016.
+  */
+class SubInstruction(label: String, op: String, val result: Int, val op1: Int, val op2: Int)
+  extends Instruction(label, op){
+
+  override def execute(m: Machine): Unit = {
+    val value1 = m.regs(op1)
+    val value2 = m.regs(op2)
+    m.regs(result) = value1 - value2
+  }
+
+  override def toString(): String = {
+    super.toString + " " + op1 + " - " + op2 + " to " + result
+  }
+}
+
+object SubInstruction {
+  def apply(label: String, result: Int, op1: Int, op2: Int) =
+    new AddInstruction(label, "sub", result, op1, op2)
+}
