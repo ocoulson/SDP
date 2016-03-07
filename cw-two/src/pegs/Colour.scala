5bfe8077b675372f18ae795ae0bf9c9c67f57ff2
ocoulson <o_coulson@hotmail.co.uk>
2016-03-07 15:12:55 +0000

    Wrote implementations of colour

--- cw-two/src/pegs/Colour.scala
+++ cw-two/src/pegs/Colour.scala
@@ -4,17 +4,28 @@ package pegs
   * Created by Oliver Coulson on 07/03/2016.
   */
 sealed trait Colour {
-  val id: Int
   val name: String
-  val symbol: Char = name.charAt(0).toUpper
+  val symbol = name.charAt(0).toUpper
 }
 
-class Red extends Colour
-class Yellow extends Colour
-class Orange extends Colour
-class Blue extends Colour
-class Green extends Colour
-class Purple extends Colour
+class Red(id: String) extends Colour {
+  override val name: String = "red"
+}
+class Orange(id: String) extends Colour {
+  override val name: String = "orange"
+}
+class Purple(id: String) extends Colour {
+  override val name: String = "purple"
+}
+class Green(id: String) extends Colour {
+  override val name: String = "green"
+}
+class Yellow(id: String) extends Colour {
+  override val name: String = "yellow"
+}
+class Blue(id: String) extends Colour {
+  override val name: String = "blue"
+}
 
 
 
