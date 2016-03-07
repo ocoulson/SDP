75e99b4b4499a080553fd6d6fadb061b2bfff0a1
ocoulson <o_coulson@hotmail.co.uk>
2016-03-07 15:40:03 +0000

    Updated ScalaDocs for ResponsePeg and created ColourHandler file containing the ColourHandler class and the ColourFactory class

---
+++ cw-two/src/handlers/ColourHandler.scala
@@ -0,0 +1,11 @@
+package handlers
+
+/**
+  * Created by Oliver Coulson on 07/03/2016.
+  */
+class ColourHandler {
+
+}
+
+
+class ColourFactory {}
--- cw-two/src/pegs/ResponsePeg.scala
+++ cw-two/src/pegs/ResponsePeg.scala
@@ -1,7 +1,11 @@
 package pegs
 
 /**
-  * Created by Oliver Coulson on 07/03/2016.
+  * ResponsePeg objects will be used to give the player feedback on their guesses. They are either black or white.
+  * A black peg will indicate a correct colour in the correct place.
+  * A white peg will indicate a correct colour but in the wrong place.
+  *
+  * Created by Oliver Coulson and George Shiangoli on 07/03/2016.
   */
 sealed trait ResponsePeg {
 
