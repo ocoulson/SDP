cb4b682fc7ecdfe7891408aa2ec13b3d97bb43c6
ocoulson <o_coulson@hotmail.co.uk>
2016-03-07 21:49:35 +0000

    Got Macwire to work by removing the core library and by using the macros library instead. Also had to refactor match statement to work on a symbol to avoid confusion between the strings for colour and the id string for the constructor. Still having issues with ClassUtil, mostly to do with SLF4J, but not only

--- cw-two/src/handlers/ColourHandler.scala
+++ cw-two/src/handlers/ColourHandler.scala
@@ -1,40 +1,38 @@
 package handlers
 
+import org.clapper.classutil.ClassFinder
 import pegs._
 /**
   * Created by Oliver Coulson on 07/03/2016.
   */
 class ColourHandler {
 
+  //TODO: Use reflection to get colourpool
+
 
   //TODO: Use ColourFactory to build and return colour objects
 
-  val colourOption = ColourFactory.newColour("red", "1")
+  val colour = ColourFactory.newColour('R', "1")
 
 
 }
 
 object main extends App{
   val ch = new ColourHandler
-  if (ch.colourOption != None) {
-    println(ch.colourOption.get.name)
-  } else
-    print(None)
+  print(ch.colour.toString)
 
 }
-
+import com.softwaremill.macwire._
 object ColourFactory {
-  def newColour(colour: String, id: String): Option[Colour] = {
+  def newColour(symbol: Char, id: String): Colour = {
 
-    colour match {
-      case "red" => return Some(new Red(id))
-      case "yellow" => return Some(new Yellow(id))
-      case "purple" => return Some(new Purple(id))
-      case "green" => return Some(new Green(id))
-      case "blue" => return Some(new Blue(id))
-      case "orange" => return Some(new Orange(id))
-
-      case _ => return None
+    symbol match {
+      case 'R' =>  wire[Red]
+      case 'Y' =>  wire[Yellow]
+      case 'P' =>  wire[Purple]
+      case 'G' =>  wire[Green]
+      case 'B' =>  wire[Blue]
+      case 'O' =>  wire[Orange]
     }
   }
 }
