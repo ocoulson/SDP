9c064eefa81dfc68b14de6e78da14a8d6a14b42d
ocoulson <o_coulson@hotmail.co.uk>
2016-03-07 12:38:54 +0000

    Imported cw-two - Started toDoList.txt for it

---
+++ cw-two/src/Game.scala
@@ -0,0 +1,8 @@
+
+trait Game {
+  /**
+    * Run a one or more game sof mastermind, until the player
+    * quits.
+    */
+  def runGames
+}
---
+++ cw-two/src/MastermindDriver.scala
@@ -0,0 +1,9 @@
+
+object MastermindDriver {
+  def main(args: Array[String]) {
+    var g: Game = Factory.getInstance(classOf[Game], true)
+    g.runGames
+    g = Factory.getInstance(classOf[Game], false)
+    g.runGames
+  }
+}
