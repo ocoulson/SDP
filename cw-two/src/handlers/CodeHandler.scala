0e8bed786709cdfab2a6e1610400393290ecbd37
ocoulson <o_coulson@hotmail.co.uk>
2016-03-10 19:37:19 +0000

    Wrote first test for CodeHandlerTest, had to change a little bit of code in CodeFactory because MacWire couldn't choose between 2 vectors to use as the constructor argument for SecretCode. Test passes

--- cw-two/src/handlers/CodeHandler.scala
+++ cw-two/src/handlers/CodeHandler.scala
@@ -9,16 +9,17 @@ import pegs.Colour
   */
 object CodeFactory {
   def generateSecretCode(pegNo: Int): SecretCode = {
+    val finalCode: Vector[Colour] = buildVector(pegNo)
+    val secretCode = wire[SecretCode]
+    secretCode
+  }
+  private def buildVector(pegNo: Int): Vector[Colour] = {
     var code: Vector[Colour] = Vector[Colour]()
     for (i <- 0 until pegNo) {
       val colour = ColourFactory.generateRandomColour().get
       code = code :+ colour
-
     }
-    val finalCode: Vector[Colour] = code
-    val secretCode = wire[SecretCode]
-    secretCode
+    code
   }
-
   def processGuess(guess: String): Guess = {???}
 }
--- cw-two/tests/CodeHandlerTest.scala
+++ cw-two/tests/CodeHandlerTest.scala
@@ -1,6 +1,13 @@
+import handlers.CodeFactory
+import org.scalatest.FunSuite
+
 /**
   * Created by Oliver Coulson on 10/03/2016.
   */
-class CodeHandlerTest {
-
+class CodeHandlerTest extends FunSuite{
+  test("A generated secretCode should be a given length") {
+    val length = 4
+    val secretCode = CodeFactory.generateSecretCode(length)
+    assert(secretCode.length == length)
+  }
 }
