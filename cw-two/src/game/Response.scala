8f243eaacbd4fd08c6c39654ce2c8f05216ad3c9
ocoulson <o_coulson@hotmail.co.uk>
2016-03-11 20:52:22 +0000

    Made a response class to hold the vector of response pegs to help with keeping the game state tidy

---
+++ cw-two/src/game/Response.scala
@@ -0,0 +1,16 @@
+
+package game
+
+import pegs.ResponsePeg
+
+/**
+  * Created by Oliver Coulson on 11/03/2016.
+  */
+class Response(pegs: Vector[ResponsePeg]) {
+  override def toString: String = {
+    val builder = new StringBuilder
+    pegs.foreach(p => builder.append(p.name + " "))
+    builder.toString()
+  }
+
+}
