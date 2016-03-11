185c2a6899c8033f29ee8b611451cb1c2e47b036
ocoulson <o_coulson@hotmail.co.uk>
2016-03-11 17:46:22 +0000

    Completed testing for getResponse method in class ResponseHandler for now. All tests currently pass.

--- cw-two/tests/ResponseHandlerTest.scala
+++ cw-two/tests/ResponseHandlerTest.scala
@@ -11,8 +11,14 @@ import pegs.{White, Black}
 class ResponseHandlerTest extends FunSuite{
   val secretCode1 = new SecretCode(Vector(ColourFactory.newColour('R').get, ColourFactory.newColour('G').get,
     ColourFactory.newColour('B').get, ColourFactory.newColour('R').get))
-  val responseHandler1 = wire[ResponseHandler]
+  val responseHandler1 = new ResponseHandler(secretCode1)
 
+  val secretCode2 = new SecretCode(Vector(ColourFactory.newColour('R').get, ColourFactory.newColour('G').get,
+    ColourFactory.newColour('B').get, ColourFactory.newColour('R').get, ColourFactory.newColour('O').get,
+    ColourFactory.newColour('P').get))
+  val responseHandler2 = new ResponseHandler(secretCode2)
+
+  //Testing 4 peg secret code
   test("The response of a correct guess should be 4 black pegs") {
     val correctGuess = new Guess(Vector(ColourFactory.newColour('R').get,
                                         ColourFactory.newColour('G').get,
@@ -98,4 +104,155 @@ class ResponseHandlerTest extends FunSuite{
     assert(whites.length == 2)
   }
 
+  test("The response of a guess with 1 fully correct and 1 partially correct should be 1 black peg and 1 white.") {
+    val guess = new Guess(Vector(ColourFactory.newColour('B').get,
+      ColourFactory.newColour('Y').get,
+      ColourFactory.newColour('P').get,
+      ColourFactory.newColour('R').get))
+
+    val response = responseHandler1.getResponse(guess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+    println(response)
+    assert(blacks.length == 1)
+    assert(whites.length == 1)
+  }
+  test("The response of a guess with 0 fully correct and 4 partially correct should 4 white pegs.") {
+    val guess = new Guess(Vector(ColourFactory.newColour('G').get,
+                                ColourFactory.newColour('R').get,
+                                ColourFactory.newColour('R').get,
+                                ColourFactory.newColour('B').get))
+
+    val response = responseHandler1.getResponse(guess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+    println(response)
+    assert(blacks.isEmpty)
+    assert(whites.length == 4)
+  }
+  test("The response of a guess with no correct should no pegs.") {
+    val guess = new Guess(Vector(ColourFactory.newColour('O').get,
+                                ColourFactory.newColour('O').get,
+                                ColourFactory.newColour('O').get,
+                                ColourFactory.newColour('O').get))
+
+    val response = responseHandler1.getResponse(guess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+    println(response)
+    assert(blacks.isEmpty)
+    assert(whites.isEmpty)
+  }
+  test("The response of a guess with 0 fully correct and 1 partially correct should be 1 white peg.") {
+    val guess = new Guess(Vector(ColourFactory.newColour('Y').get,
+                                ColourFactory.newColour('B').get,
+                                ColourFactory.newColour('Y').get,
+                                ColourFactory.newColour('Y').get))
+
+    val response = responseHandler1.getResponse(guess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+    println(response)
+    assert(blacks.isEmpty)
+    assert(whites.length == 1)
+  }
+
+  //Testing 6 peg secret code
+  test("The response of a correct guess should be 6 black pegs") {
+    val correctGuess = new Guess(Vector(ColourFactory.newColour('R').get,
+                                        ColourFactory.newColour('G').get,
+                                        ColourFactory.newColour('B').get,
+                                        ColourFactory.newColour('R').get,
+                                        ColourFactory.newColour('O').get,
+                                        ColourFactory.newColour('P').get))
+
+    val response = responseHandler2.getResponse(correctGuess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+
+    println(response)
+    assert(response.length == secretCode2.length)
+  }
+
+  test("The response of no correct guess should be no pegs") {
+    val incorrectGuess = new Guess(Vector(ColourFactory.newColour('Y').get,
+                                          ColourFactory.newColour('Y').get,
+                                          ColourFactory.newColour('Y').get,
+                                          ColourFactory.newColour('Y').get,
+                                          ColourFactory.newColour('Y').get,
+                                          ColourFactory.newColour('Y').get))
+
+    val response = responseHandler2.getResponse(incorrectGuess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+
+    println(response)
+    assert(blacks.isEmpty)
+    assert(whites.isEmpty)
+  }
+
+  test("The response of 5 correct guess should be 5 black pegs") {
+    val incorrectGuess = new Guess(Vector(ColourFactory.newColour('Y').get,
+      ColourFactory.newColour('G').get,
+      ColourFactory.newColour('B').get,
+      ColourFactory.newColour('R').get,
+      ColourFactory.newColour('O').get,
+      ColourFactory.newColour('P').get))
+
+    val response = responseHandler2.getResponse(incorrectGuess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+
+    println(response)
+    assert(blacks.length == 5)
+  }
+
+  test("The response of 4 correct guesses and 1 partial correct guess should be 4 black pegs and 1 white peg") {
+    val incorrectGuess = new Guess(Vector(ColourFactory.newColour('P').get,
+      ColourFactory.newColour('G').get,
+      ColourFactory.newColour('B').get,
+      ColourFactory.newColour('R').get,
+      ColourFactory.newColour('O').get,
+      ColourFactory.newColour('Y').get))
+
+    val response = responseHandler2.getResponse(incorrectGuess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+
+    println(response)
+    assert(blacks.length == 4)
+    assert(whites.length == 1)
+  }
+
+  test("The response of 6 partial correct guesses should be 6 white peg") {
+    val incorrectGuess = new Guess(Vector(ColourFactory.newColour('P').get,
+      ColourFactory.newColour('R').get,
+      ColourFactory.newColour('G').get,
+      ColourFactory.newColour('B').get,
+      ColourFactory.newColour('R').get,
+      ColourFactory.newColour('O').get))
+
+    val response = responseHandler2.getResponse(incorrectGuess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+
+    println(response)
+    assert(blacks.isEmpty)
+    assert(whites.length == 6)
+  }
+
+  test("The response of 3 correct guesses and 3 partial correct guesses should be 3 black pegs and 3 white pegs") {
+    val incorrectGuess = new Guess(Vector(ColourFactory.newColour('R').get,
+      ColourFactory.newColour('R').get,
+      ColourFactory.newColour('B').get,
+      ColourFactory.newColour('P').get,
+      ColourFactory.newColour('O').get,
+      ColourFactory.newColour('G').get))
+
+    val response = responseHandler2.getResponse(incorrectGuess)
+    val blacks = response.filter(rp => rp.isInstanceOf[Black])
+    val whites = response.filter(rp => rp.isInstanceOf[White])
+
+    println(response)
+    assert(blacks.length == 3)
+    assert(whites.length == 3)
+  }
 }
