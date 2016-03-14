9e2cddd65301a8660b81319232c2bfde441d126f
ocoulson <o_coulson@hotmail.co.uk>
2016-03-14 17:13:53 +0000

    ScalaDoc comments added to all classes within handlers package.

--- cw-two/src/handlers/ColourHandler.scala
+++ cw-two/src/handlers/ColourHandler.scala
@@ -9,7 +9,6 @@ import scala.util.Random
 
 /**
   * A class ColourHandler which is responsible for getting the available colours via reflection
-  * An object ColourFactory which is responsible for instantiating colour objects on request.
   *
   * Created by Oliver Coulson and George Shiangoli on 07/03/2016.
   */
@@ -24,6 +23,10 @@ class ColourHandler {
   val instanceNames: List[String] = subclasses.map(i => i.name).toList
   val rawNames = instanceNames.map(i => i.substring(i.indexOf(".")+1))
 
+  /**
+    * Returns the rawNames value as a slightly formatted string for use in the game blurb
+    * @return the slightly formatted string
+    */
   override def toString: String = {
     val rawString = rawNames.toString()
     rawString.substring(rawString.indexOf('(')+1, rawString.indexOf(')'))
@@ -31,9 +34,20 @@ class ColourHandler {
 }
 
 
-
+/**
+  * An object ColourFactory which is responsible for instantiating colour objects on request.
+  */
 object ColourFactory {
   val colourHandler = wire[ColourHandler]
+
+  /**
+    * When given a character, if that character is the first character of a recognised class that
+    * extends Colour, the corresponding Colour object is instantiated and returned, wrapped in an
+    * Option. If the character does not correspond to a Colour, an empty Option is returned.
+    *
+    * @param name the Char corresponding to a recognised Colour
+    * @return An Option containing either the Colour corresponding to the input Char, or None
+    */
   def newColour(name: Char): Option[Colour] = {
 
       if(colourHandler.rawNames.map(_.charAt(0).toUpper).contains(name)) {
@@ -44,6 +58,11 @@ object ColourFactory {
       } else None
   }
 
+  /**
+    * This returns a random Colour, selected from the list of available Colours found in the game,
+    * wrapped in an option.
+    * @return A Colour wrapped in an Option
+    */
   def generateRandomColour(): Option[Colour] = {
     val colourHandler = wire[ColourHandler]
     val colours = colourHandler.rawNames
