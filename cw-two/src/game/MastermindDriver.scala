f7cff2e6e999f2f770ffdae762e8f39c03d2df6a
ocoulson <o_coulson@hotmail.co.uk>
2016-03-14 14:34:49 +0000

    Added a pattern matching statement to the MastermindDriver class to allow user to choose easy game with code displayed or hard with code not displayed

--- cw-two/src/game/MastermindDriver.scala
+++ cw-two/src/game/MastermindDriver.scala
@@ -1,12 +1,20 @@
 package game
 
+import scala.io.StdIn
+
 /**
   * Created by Oliver Coulson on 10/03/2016.
   */
 object MastermindDriver {
   def main(args: Array[String]) {
-    //Input argument 'true' for easy, 'false' for hard
-    val g: Game = Factory.getInstance(true)
-    g.runGames
+    println("Easy or hard - Input E for easy, or anything else for hard")
+    print(":>")
+    val input = StdIn.readLine().toUpperCase
+    input match {
+      case "E" => val g: Game = Factory.getInstance(true)
+        g.runGames
+      case _ => val g: Game = Factory.getInstance(false)
+        g.runGames
+    }
   }
 }
